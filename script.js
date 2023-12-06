426069607fc34cbb0686c0662e46c958cd32e126
VolTorian <33360939+VolTorian@users.noreply.github.com>
2023-12-05 22:10:36 -0800

    functionality to add book

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const myLibrary = [];
+
+function Book(author, title, pageCount, isRead) {
+    this.author = author;
+    this.title = title;
+    this.pageCount = pageCount;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+    let author = prompt("Enter the author of the book:");
+    let title = prompt("Enter the book's title:");
+    let pageCount = prompt("Enter the number of pages the book has:");
+    let isRead = prompt("Have you read the book already? [y/n]:");
+    myLibrary.push(new Book(author, title, pageCount, isRead));
+}
